b47bfde0fbf377bafe6dd918f13464b96714d3e3
unknown <javierazd1305@gmail.com>
2020-10-09 03:10:23 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Home from "./components/Home/index";
 import Feature from "./components/Feature/index";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
